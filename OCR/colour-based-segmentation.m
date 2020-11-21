b8c6a11a698e48578b02a3c4e5c969bde9d3b2e0
Francesca Bettinelli <70513072+FraBet29@users.noreply.github.com>
2020-11-21 11:08:40 +0100

    Update colour-based-segmentation.m

--- OCR/colour-based-segmentation.m
+++ OCR/colour-based-segmentation.m
@@ -4,7 +4,9 @@ figure()
 imshow(image);
 
 % Convert image from RGB color space to L*a*b* color space
-lab = rgb2lab(image); ab = lab(:,:,2:3); % For our purposes, we ignore L (lightness) 
+% The L*a*b* space consists of a luminosity layer 'L*', chromaticity-layer 'a*' indicating where color falls along the red-green axis,
+% and chromaticity-layer 'b*' indicating where the color falls along the blue-yellow axis
+lab = rgb2lab(image); ab = lab(:,:,2:3); % For our purposes, we ignore L 
 ab = im2single(ab); % Convert to data type 'single' necessary to compute imsegkmeans
 
 % Choose number of clusters
